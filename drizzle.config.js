81ee6acda2b5ab3a79b2381be7a29fa7392d0f64
AmiteshSingh45 <amiteshsinghrajput00831@gmail.com>
2025-07-10 17:41:54 +0530

    connected to neon drizzle database

---
+++ drizzle.config.js
@@ -0,0 +1,9 @@
+export default ({
+  dialect: "postgresql",
+  schema: "./config/schema.js",
+  dbCredentials: {
+    url:'postgresql://neondb_owner:npg_s87btnCFpGrJ@ep-young-cloud-abj05oso-pooler.eu-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
+  }
+});
+
+
